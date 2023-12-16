5a9fbd9eb8740d53c7f504f560d8836a703af8b2
matslyk0 <matslyk0@gmail.com>
2023-12-16 18:38:49 +0000

    Adding WADE Demo and myenv

---
+++ WADE_mk2_demo.py
@@ -0,0 +1,64 @@
+"""
+please note that this code is not functional as I have removed the API keys
+"""
+
+import os, requests, random
+from openai import OpenAI
+from PIL import Image
+from pyowm.owm import OWM
+
+#API Keys
+owm = OWM('')
+OPENAI_API_KEY=''
+
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+client = OpenAI()
+
+wthr_manager = owm.weather_manager()
+geo_manager = owm.geocoding_manager()
+
+def get_image(user_prompt, image_dimension='1024x1024', image_quality='standard', model='dall-e-3', nb_final_image=1):
+    response = client.images.generate(
+        model=model,
+        prompt=user_prompt,
+        size=image_dimension,
+        quality=image_quality,
+        n=nb_final_image,)
+
+    image_url = response.data[0].url
+
+    my_img = Image.open(requests.get(image_url, stream=True).raw)
+
+    my_img.show()
+
+city_array = []
+while len(city_array) == 0:
+
+    cityInput = input('Please enter a city: ')
+    while len(cityInput) == 0:
+        cityInput = input('Nothing entered, please try again: ')
+
+    countryCode = input('Please enter the associated country ISO code: ')
+    if countryCode.lower() == 'us':
+        stateCode = input('Please enter the state code associated with the location: ')
+        city_array = geo_manager.geocode(cityInput,countryCode,stateCode)
+
+    while len(countryCode) != 2:
+        countryCode = input('Please enter the associated country ISO code: ')
+        
+    city_array = geo_manager.geocode(cityInput,countryCode)
+    if len(city_array) == 0:
+        print('Sorry, no results came up. Please enter the information again.')
+
+city = city_array[0]
+
+one_call = wthr_manager.one_call(city.lat,city.lon)
+temperature_info = one_call.current.temperature('celsius')
+temperature = temperature_info['temp']
+humidity = one_call.current.humidity
+
+print("Loading...")
+
+get_image(f"{city} with a temperature of {temperature} degrees celsius and a humidity of {humidity}")
+
+# add specific weather types (rain, sunshine etc.)
