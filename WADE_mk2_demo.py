5d76d9eca7d316bbf5fcf2767e67c1a346d84cde
matslyk0 <matslyk0@gmail.com>
2024-06-27 14:57:27 +0100

    housekeeping
    - API keys are now accessed through my system's environment variables, so I can now share this code without having to remove the keys prior.
    
    - Temporarily added a (messy) option to terminate the program midway, for testing purposes.
    
    - Simplified the response variable in the get_image function, improving readability. The functionality of the previous version is not needed for current purposes.

--- WADE_mk2_demo.py
+++ WADE_mk2_demo.py
@@ -1,29 +1,20 @@
-"""
-please note that this code is not functional as I have removed the API keys
-"""
-
 import os, requests, random
 from openai import OpenAI
 from PIL import Image
 from pyowm.owm import OWM
 
 #API Keys
-owm = OWM('')
-OPENAI_API_KEY=''
-
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-client = OpenAI()
+owm = OWM(os.environ.get("OWM_API_KEY")) # grabbing the key from the environment variable on my system 
+client = OpenAI() # the api_key argument is automatically obtained from the OPENAI_API_KEY environment variable
 
 wthr_manager = owm.weather_manager()
 geo_manager = owm.geocoding_manager()
 
-def get_image(user_prompt, image_dimension='1024x1024', image_quality='standard', model='dall-e-3', nb_final_image=1):
-    response = client.images.generate(
-        model=model,
-        prompt=user_prompt,
-        size=image_dimension,
-        quality=image_quality,
-        n=nb_final_image,)
+def get_image(user_prompt):
+    """
+    Prompts DALL-E 3 with the user's prompt (str).
+    """
+    response = client.images.generate(model='dall-e-3', prompt=user_prompt, size='1024x1024', quality="standard", n=1)
 
     image_url = response.data[0].url
 
@@ -37,6 +28,8 @@ while len(city_array) == 0:
     cityInput = input('Please enter a city: ')
     while len(cityInput) == 0:
         cityInput = input('Nothing entered, please try again: ')
+    if cityInput == "BREAK":
+        break
 
     countryCode = input('Please enter the associated country ISO code: ')
     if countryCode.lower() == 'us':
@@ -60,5 +53,3 @@ humidity = one_call.current.humidity
 print("Loading...")
 
 get_image(f"{city} with a temperature of {temperature} degrees celsius and a humidity of {humidity}")
-
-# add specific weather types (rain, sunshine etc.)
